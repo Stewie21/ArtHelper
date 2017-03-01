295156531f7246b986fac996c4bc8e83702b0a19
Ivan Ivaleev <ivaleev1997@gmail.com>
2017-03-02 00:03:52 +0300

    weather complete 60%

--- app/src/main/java/com/example/arteme/myapplication/weather/data/Item.java
+++ app/src/main/java/com/example/arteme/myapplication/weather/data/Item.java
@@ -1,4 +1,15 @@
 package com.example.arteme.myapplication.weather.data;
 
 public class Item {
+    private int atmosphere;
+    private int temperature;
+    private int windDirection;
+    private int windSpeed;
+
+    public Item(int atm, int temp, int windD, int windS) {
+        atmosphere = atm;
+        temperature = temp;
+        windDirection = windD;
+        windSpeed = windD;
+    }
 }
